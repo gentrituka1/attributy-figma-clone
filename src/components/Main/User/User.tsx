c3459157f8cbfa9b52977f5b3f603d1f2d0ce60f
Gentrit Uka <gentrit-uka@live.com>
2023-02-05 23:02:19 +0100

    updated

--- src/components/Main/User/User.tsx
+++ src/components/Main/User/User.tsx
@@ -10,12 +10,12 @@ function User({ user }: Props) {
   return (
     <div className="main-user">
       <input type="checkbox" />
-      <p>{user.name}</p>
-      <p>{user.email}</p>
-      <p>{user.companyName}</p>
-      <p>{user.role}</p>
-      <p>{user.recentActivity}</p>
-      <p></p>
+      <div className="main-user__name flex2"><p>{user.name}</p></div>
+      <div className="main-user__email flex2"><p>{user.email}</p></div>
+      <div className="main-user__company flex2"><p>{user.companyName}</p></div>
+      <div className="main-user__role flex2"><p>{user.role}</p></div>
+      <div className="main-user__activity flex2"><p>{user.recentActivity}</p></div>
+      <div><p></p></div>
     </div>
   );
 }
